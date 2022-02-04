6c30eaa1e1814d281a1bfa39a7c3f512dfc2236a
Hong <tjdqls1668@ajou.ac.kr>
2022-02-04 18:51:55 +0900

    DOCS: payload content

--- payload/etc.ts
+++ payload/etc.ts
@@ -5,16 +5,16 @@ const etc: IEtc.Payload = {
 
   list: [
     {
+      title: '삼성 S/W 알고리즘 역량강화 과정',
+      subTitle: '',
+      startedAt: '2022-01',
+      endedAt: '2022-02',
+    },
+    {
       title: 'TOPCIT 제 16회 정기평가',
       subTitle: '508점',
       startedAt: '2021-10',
     },
-    // {
-    //   title: 'IT Education Service Activities',
-    //   subTitle: 'Wrold Friends IT Volunteer',
-    //   startedAt: '2013-07',
-    //   endedAt: '2013-08',
-    // },
     // {
     //   title: 'Information Olympiad',
     //   subTitle: 'Silver Prize',
